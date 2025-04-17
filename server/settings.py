ccf6646366b7334508fbf09978912961f80021d8
shakibosd <syednazmusshakib94@gmail.com>
2025-04-17 23:47:38 +0600

    feat(subscribe): subscribe model verification_token link added

--- server/settings.py
+++ server/settings.py
@@ -168,7 +168,7 @@ DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 EMAIL_BACKEND="django.core.mail.backends.smtp.EmailBackend"
 SITE_ID=1
 
-EMAIL_HOST = "smtp.gmail.com"
+EMAIL_HOST = "smtp.gmail.com"   
 EMAIL_PORT = 587
 EMAIL_USE_TLS = True
 EMAIL_HOST_USER = env("EMAIL")
